87f6ad2e30a877d4490aad67bfcf544819e80dfe
Roneil Bansas <Roneilbansas5222@gmail.com>
2021-07-18 23:30:12 -0700

    second commit address

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
      <p>Hello world</p>
+     <h1>I'm Roneil Bansas</h1>
+     <address>Pob. Mahayag Zamboanga Del Sur</address>
     </div>
   );
 }
