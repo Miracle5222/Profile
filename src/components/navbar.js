16b36ffe7c0592d912ffeb0ec9c5dc913f8a4daa
Roneil Bansas <Roneilbansas5222@gmail.com>
2021-08-08 01:08:48 -0700

    third commit adding social media buttons

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,24 +3,39 @@ import React from 'react'
 import { Link, } from "react-router-dom";
 import './nav-style.scss';
 import Button from './button';
-
+import SocialMedia from './socialMedia';
 const Navbar = () => {
+
+    function handleClick() {
+        document.getElementById('con').style.width = '-200px';
+    }
     return (
 
-        <div className='container'>
-            <div className='container__logo'>
-                <h1>Noblesse</h1>
+        <div>
+
+            <div className={`container `} id='con'>
+                <div className='container__logo'>
+                    <h1>Noblesse</h1>
+                </div>
+                <div className='container__menu'>
+                    <ul>
+                        <li><a href='#'>Home</a></li>
+                        <li><a href='#'>Services</a></li>
+                        <li><a href='#'>Profile</a></li>
+                        <li><a href='#'>Projects</a></li>
+                        <li><a href='#'>About</a></li>
+                    </ul>
+                </div>
+                <Button />
+
+                <button className='container__menu-item' onclick={handleClick}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
+
             </div>
-            <div className='container__menu'>
-                <ul>
-                    <li><a href='#'>Home</a></li>
-                    <li><a href='#'>Services</a></li>
-                    <li><a href='#'>Resourcess</a></li>
-                    <li><a href='#'>Projects</a></li>
-                    <li><a href='#'>About</a></li>
-                </ul>
-            </div>
-            <Button />
+
         </div>
 
     )
--- src/components/socialMedia.js
+++ src/components/socialMedia.js
@@ -4,21 +4,24 @@ import './socialMedia.scss';
 function SocialMedia() {
     return (
         <div className='sm'>
-            <a href='https://www.facebook.com/' target='_blank'>
-                <div className='m sm__facebook'>
-
+            <div className='sm__facebook'>
+                <div className='sm__facebook-logo cursor'>
+                    
                 </div>
-            </a>
-            <a href='https://www.instagram.com/' target='_blank'>
-                <div className='m sm__instagram'>
-                </div>
-            </a>
 
-            <a href='https://www.linkedin.com/in/roneil-bansas-4a75441a1/' target='_blank'>
-                <div className='m sm__linkedIn'>
+            </div>
+            <div className='sm__instagram'>
+                <div className='sm__instagram-logo cursor'>
+                    
                 </div>
-            </a>
 
+            </div>
+            <div className='sm__linkedin'>
+                <div className='sm__linkedin-logo cursor'>
+                  
+                </div>
+
+            </div>
         </div >
     )
 }
